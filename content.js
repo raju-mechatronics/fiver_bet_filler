cf1b1d884ef77c21e0937c86f3c96deb2a14a061
Raju Ahmed <73926176+raju-mechatronics@users.noreply.github.com>
2022-04-06 02:35:18 +0600

    content

--- content.js
+++ content.js
@@ -1,13 +1,13 @@
 function setCombo(combo) {
   const comboCheckbox = document.querySelectorAll(".legCheckbox input");
-  const combos = combo.split("+").map((e) => Number(e) - 1);
+  const combos = combo.split("-").map((e) => Number(e) - 1);
   combos.forEach((combo) => {
     if (!comboCheckbox[combo] || comboCheckbox[combo].disabled) {
       return false;
     }
   });
-  combos.forEach((combo) => {
-    comboCheckbox[combo].click();
+  combos.forEach((com) => {
+    comboCheckbox[com].click();
   });
   return true;
 }
@@ -41,7 +41,14 @@ async function wait(ms) {
   return new Promise((res) => setTimeout(res, ms));
 }
 
+function isFCT() {
+  return !!location.pathname.includes("FCT");
+}
+
 async function addSingleBet({ combo, amount, pool }) {
+  if (isFCT()) {
+  }
+
   if (!setCombo(combo)) return;
   if (!setType(pool)) return;
   setAmount(amount);
@@ -50,17 +57,58 @@ async function addSingleBet({ combo, amount, pool }) {
 }
 
 async function fillForm(data) {
-  for (const datum in data) {
+  console.log(data);
+  for (const datum of data) {
+    console.log(datum);
     addSingleBet(datum);
     await wait(10);
   }
 }
 
-chrome.runtime.onMessage.addListener((req) => {
+async function fillBatchForm(data) {
+  let j = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (j == 29) {
+      j = 0;
+      send();
+      await wait(1000);
+    }
+    addSingleBet(data[i]);
+    await wait(10);
+    j++;
+  }
+}
+
+chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
   if (req.message === "fillForm") {
-    fillForm(req.data);
+    if (req.data.length > 30) {
+      fillBatchForm(req.data);
+    } else fillForm(req.data);
   }
   if (req.message === "send") {
     send();
   }
+  if (req.message === "url") {
+    console.log(document.url);
+    const query = document.URL.split("?")[1].split("&");
+    let params = {};
+    query.forEach(function (e) {
+      params[e.split("=")[0]] = e.split("=")[1];
+    });
+    sendResponse(params);
+  }
 });
+
+console.log("running");
+
+let extensionOrigin = "chrome-extension://" + chrome.runtime.id;
+if (!location.ancestorOrigins.contains(extensionOrigin)) {
+  let iframe = document.createElement("iframe");
+  // Must be declared at web_accessible_resources in manifest.json
+  iframe.src = chrome.runtime.getURL("popup.html");
+  // Some styles for a fancy sidebar
+  iframe.style.cssText =
+    "position:fixed;top:0;right:0;display:block;" +
+    "width:680px;height:100%;z-index:1000;";
+  document.body.appendChild(iframe);
+}
